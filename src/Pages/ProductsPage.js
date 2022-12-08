1795cc4a29dd2207119edf93129c2f4fe6b2e952
Clayton  Wright <81819518+Cdwright93@users.noreply.github.com>
2022-12-08 05:23:59 -0500

    content hook working well, needs styling

--- src/Pages/ProductsPage.js
+++ src/Pages/ProductsPage.js
@@ -3,47 +3,27 @@ import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useContent } from "../Hooks/Content";
 
-
-
-//using an content provider to get the products
 const Products = (props) => {
 	const navigate = useNavigate();
 	const { products } = useContent();
-	const [search, setSearch] = useState("");
-	const searchInput = useRef();
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		navigate(`/products?search=${search}`);
-		searchInput.current.value = "";
-	};
-
 	return (
 		<div className="App-header">
+			<div>
 			<h1 className="page-h1">Products</h1>
-			<form onSubmit={handleSubmit}>
-				<input
-					type="text"
-					placeholder="Search"
-					ref={searchInput}
-					onChange={(e) => setSearch(e.target.value)}
-				/>
-				<button type="submit">Search</button>
-			</form>
+			</div>
 			{products.map((product) => (
 				<div key={product._id} id={product._id} className="product-card">
-					<p className="page-p">{product.name}</p>
-					<img src={product.image} alt={product.name} />
-					<p className="page-p">{product.description}</p>
-					<p className="page-p">{product.price}</p>
-					<button onClick={() => navigate(`/products/${product.id}`)}>
-						Details
+					<p className="product-title">{product.name}</p>
+					<img src={product.image} alt={product.name} className="product-image"/>
+					<p className="product-p">{product.description}</p>
+					<p className="product-p">${product.price}.00/month</p>
+					<button className='product-button'onClick={() => navigate(`/products/${product.id}`)}>
+						<p className='product-button-p'>See more</p>
 					</button>
 				</div>
 			))}
 		</div>
 	);
-
 };
 
 export default Products;
