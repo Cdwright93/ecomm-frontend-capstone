00233cbd5327ae25d7ce336aae1400d3719d60c8
Clayton  Wright <81819518+Cdwright93@users.noreply.github.com>
2022-12-09 23:29:16 -0500

    more styling

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -28,11 +28,13 @@ const Home = (props) => {
 		}
 		return randomWeirdSymbols;
 	}
-	//generate some ipsum lorem text in the p tag below
+	
 	return (
 		<div className="pageheader">
-			<div>
+			<div className="symbol-decorations">
 				<h3 className="page-h1">{randomizeWeirdSymbols(4)}</h3>
+				<h3 className="page-h2">Home</h3>
+				<h3 className="page-h1-bottom">{randomizeWeirdSymbols(4)}</h3>
 				{user ? (
 				<div className="home-header">
 					<h3 className="page-h3">Welcome back, {user.userdata.firstname}</h3>
@@ -42,8 +44,7 @@ const Home = (props) => {
 						<p className="page-p">
 						Lorem Ipsum
 "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
-
+"What is the line between Art and dark magic?"
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac bibendum quam, ac fringilla mauris. Donec molestie facilisis neque, non pellentesque lacus elementum ut. Donec condimentum in nisl in interdum. Sed non purus ut lacus facilisis egestas. Fusce at vulputate enim. Mauris vel facilisis lacus. Mauris congue, lorem at ullamcorper posuere, tellus nisl auctor metus, a auctor justo augue semper velit. Suspendisse tincidunt convallis pellentesque. Aenean eu eleifend nunc. Suspendisse cursus nisi ipsum, ac vulputate turpis mattis sit amet. Duis commodo nec quam nec porttitor. Nulla mollis leo dui, vel gravida arcu blandit nec. Cras eleifend posuere lorem, sed finibus sapien efficitur ut. Quisque condimentum posuere facilisis. Nunc tempus viverra nunc, et aliquam libero. Aliquam venenatis dui at aliquam vehicula.
 						</p>
 						<p className="w-sym">
